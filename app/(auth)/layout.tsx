c8ca7bc0f72827d590c999abcefc398e1a3eb478
danielrr25 <danielrr25@hotmail.com>
2024-06-03 18:04:01 -0400

    implement clerk webhooks

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,5 +1,8 @@
 import React from 'react'
 
+
+// Layout component for authentication pages, providing a consistent layout.
+
 // Layout has always export some children within them
 const Layout = ({ children }: { children: React.ReactNode}) => {
   return (
