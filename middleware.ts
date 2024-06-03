ca60fc0687bc302794b3ec82c582184e43e0f4cd
danielrr25 <danielrr25@hotmail.com>
2024-06-03 11:40:24 -0400

    implement clerk webhooks

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,21 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 
-export default clerkMiddleware();
+// Define public routes
+const isPublicRoute = createRouteMatcher([
+  '/',
+  '/api/webhooks/clerk',
+  // '/api/webhooks/stripe',
+]);
+
+export default clerkMiddleware((auth, req) => {
+  // Protect all routes except the ones defined as public
+  if (!isPublicRoute(req)) {
+    auth().protect();
+  }
+});
 
 export const config = {
-  matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
+  matcher: [
+    '/((?!.*\\..*|_next|favicon.ico).*)', '/', '/(api|trpc)(.*)',
+  ],
 };
